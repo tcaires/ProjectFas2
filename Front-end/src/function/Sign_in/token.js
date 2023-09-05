6c8b539a40d92272c3c029deb1304bb7078f72d1
tcaires <thiago.caires@sec4you.com.br>
2023-09-05 17:01:14 -0300

    Atualização 12.2

--- Front-end/src/function/Sign_in/token.js
+++ Front-end/src/function/Sign_in/token.js
@@ -7,8 +7,8 @@ export default async function criaToken() {
     data.append('grant_type', 'password');
     data.append('client_id', '7f522efb-d91e-4c6d-88ba-d8d545a4d379');
     data.append('client_secret', '80f65353-3565-4c5e-ab92-497376a3ba02');
-    data.append('username', 'thiago');
-    data.append('password', 'el%O6WVy');
+    data.append('username', '******');
+    data.append('password', '*******');
 
     const accessURL = 'https://ec2-18-224-170-21.us-east-2.compute.amazonaws.com:8443/auth/oauth/v2/token';
 
